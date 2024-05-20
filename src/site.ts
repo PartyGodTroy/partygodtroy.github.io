4f23951764a765dd41a0cfc9ebeb6bc56cb8d54b
PartyGodTroy <kidveno@live.com>
2024-05-20 07:48:52 -0400

    new setup

--- src/site.ts
+++ src/site.ts
@@ -1,11 +1,8 @@
-export default {
-    title: 'My Site',
-    description: 'This is my site',
-    keywords: 'site, my site, my site description',
-    nav_htmx:{
-        "hx-boost":"true",
-        "hx-target":"main",
-        "hx-select":"main", 
-        "hx-swap":"outerHTML transition:true"
-    }
+export interface Post{
+    title: string;
+    description: string;
+    tags: string[];
+    slug: string;
+    featured_img:{ url: string; alt: string; width?: number; height?: number;}
+    date: number;
 }
