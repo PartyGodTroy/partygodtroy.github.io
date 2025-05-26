17a50e844db68b16a575b2e6f5fe5fbc0485d5a9
PartyGodTroy <kidveno@live.com>
2025-05-26 16:02:20 -0400

    upgraded tw added skills

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
 import htmx from 'astro-htmx';
 import react from "@astrojs/react";
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
-  site: 'https://partygodtroy.github.io'
+  integrations: [ react()],
+  site: 'https://partygodtroy.github.io',
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
--- src/components/Skill.tsx
+++ src/components/Skill.tsx
@@ -1,17 +1,36 @@
-export default (props:{name: string, points: number}) => {
-  const pointsArray = Array.from({ length: props.points }, (_, i) => i + 1);
+export default (props:{name: string, points: number, years?:number}) => {
+  const pointsArray = Array.from({ length: 10}, (_, i) => i + 1);
+
+  const colors =[
+    'bg-red-500',
+    'bg-orange-500',
+    'bg-orange-400',
+    'bg-yellow-500',
+    'bg-yellow-400',
+    'bg-yellow-300',
+    'bg-green-200',
+    'bg-green-300',
+    'bg-green-400',
+    'bg-green-500',
+
+
+  ]
+
   return (
     <div className="skill">
-      <h3 className="text-xl font-bold">{props.name}</h3>
-      <div className="flex gap-2 bg-zinc-800 border border-zinc-700 p-2 rounded-lg w-fit">
+      <h4 className="text-xl font-bold">{props.name} {props.years && <span className="text-lg">({props.years} yrs)</span>}</h4>
+      <div className=" bg-zinc-800 border border-zinc-700 p-2 rounded-lg w-fit">
+        <div className="flex gap-2">
         {pointsArray.map((point) => (
           <div
             key={point}
-            className="rounded min-w-5 min-h-5 bg-blue-600"
+            className={`rounded min-w-5 min-h-5 ${ point > props.points ? 'bg-transparent' : colors[point - 1]}`}
           >
           </div>
         ))}
       </div>
+        </div>
+
     </div>
   );
 };
